434fbd36bef5567e162ba1a44309b213385f5b21
Krzysztof Płachta <qrooel@gmail.com>
2023-09-26 18:12:55 +0200

    Initial commit

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+CarrierWave.configure do |config|
+  config.root = Rails.root
+end
---
+++ spec/factories/attachments.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: attachments
+#
+#  id         :bigint           not null, primary key
+#  file       :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :attachment do
+    user { create(:user) }
+    file { Rack::Test::UploadedFile.new('spec/fixtures/files/test.txt', 'text/plain') }
+  end
+end
